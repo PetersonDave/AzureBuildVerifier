927cb3cb3d8ec912d9f4d20c7ad1690581bc9145
David Peterson <dpeterson@isitedesign.com>
2014-08-01 23:10:44 -0400

    Client command, package and logic.

---
+++ Source/AzureBuildVerifier/Commands/VerifyBuild.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+using Sitecore.Azure.Deployments.AzureDeployments;
+using Sitecore.Shell.Framework.Commands;
+
+namespace AzureBuildVerifier.Commands
+{
+    internal class VerifyBuild : Command
+    {
+        private const string DialogUrl = "/modifications/test.aspx";
+
+        public override void Execute(CommandContext context)
+        {
+            if (context.Items.Length != 1) return;
+
+            var item = context.Items[0];
+            var parameters = new NameValueCollection();
+            parameters["id"] = item.ID.ToString();
+
+            Sitecore.Context.ClientPage.Start(this, "Run", parameters);
+        }
+
+        protected void Run(Sitecore.Web.UI.Sheer.ClientPipelineArgs args)
+        {
+            var url = new Sitecore.Text.UrlString(DialogUrl);
+            url.Append("id", args.Parameters["id"]);
+
+            Sitecore.Context.ClientPage.ClientResponse.ShowModalDialog(url.ToString());
+        }
+
+        public override CommandState QueryState(CommandContext context)
+        {
+            bool isTemplateTypeDeployment = context.Items.Any(item => item.TemplateID == AzureDeploymentItem.TemplateID);
+            return isTemplateTypeDeployment ? base.QueryState(context) : CommandState.Hidden;
+        }
+    }
+}
